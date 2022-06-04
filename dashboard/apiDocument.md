1ba0f76d1398d914d8728a0037b138848a46f9a8
aakashdeveloper <ahanda205@gmail.com>
2022-06-04 22:47:04 +0530

    api

---
+++ dashboard/apiDocument.md
@@ -0,0 +1,13 @@
+> Add User
+# (Post) http://localhost:9700/addUser/
+> Update User
+# (Put) http://localhost:9700/updateUser/
+> Get User
+# (Get) http://localhost:9700/users
+> Delete User
+# (Delete) http://localhost:9700/deleteUser/
+> Activate User
+# (Patch) http://localhost:9700/activateUser/
+> Deactivate User
+# (Patch) http://localhost:9700/deactivateUser/
+> Change User Role User
---
+++ dashboard/apiWithoutUi.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9700;
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoUrl = "mongodb://localhost:27017";
+let db;
+let col_name = "dashboard";
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/health',(req,res)=>{
+    res.status(200).send('Health Check ok')
+})
+
+app.get('/users',(req,res) => {
+    let query = {};
+    if(req.query.role && req.query.city){
+        query={role:req.query.role,city:req.query.city,isActive:true}
+    }
+    else if(req.query.role){
+        query={role:req.query.role,isActive:true}
+    }
+    else if(req.query.city){
+        query={city:req.query.city,isActive:true}
+    }
+    else if(req.query.isActive){
+        let isActive = req.query.isActive;
+        if(isActive == "false"){
+            isActive = false
+        }else{
+            isActive = true
+        }
+        //query = {isActive:isActive}
+        query = {isActive}
+    }else{
+        query = {isActive:true}
+    }
+    
+    db.collection(col_name).find(query).toArray((err,result) => {
+        if(err) throw err;
+        res.status(200).send(result)
+    })
+})
+
+//find particular user
+app.get('/user/:id',(req,res) => {
+    let id = mongo.ObjectId(req.params.id)
+    db.collection(col_name).find({_id:id}).toArray((err,result) => {
+        if(err) throw err;
+        res.status(200).send(result)
+    })
+})
+
+// add user
+app.post('/addUser',(req,res) => {
+    db.collection(col_name).insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.status(200).send('Data Added')
+    })
+})
+
+//update User
+app.put('/updateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                name:req.body.name,
+                city:req.body.city,
+                phone:req.body.phone,
+                role:req.body.role,
+                date:new Date(),
+                isActive:true
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('User Updated')
+        }
+    )
+})
+
+//hard Delete
+app.delete('/deleteUser',(req,res) => {
+    db.collection(col_name).remove(
+        {_id:mongo.ObjectId(req.body._id)},(err,result) => {
+            if(err) throw err;
+            res.send('User Deleted')
+        }
+    )
+})
+
+//soft Delete // deactivate
+app.patch('/deactivateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                isActive:false
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('User Deactivated')
+        }
+    )
+})
+
+//soft Delete // Activate
+app.patch('/activateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                isActive:true
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('User Activated')
+        }
+    )
+})
+
+MongoClient.connect(mongoUrl,(err,client) => {
+    if(err) console.log(`Error While Connecting`)
+    db = client.db('edumay');
+    app.listen(port,(err) => {
+        console.log(`Server is running on port ${port}`)
+    })
+})
